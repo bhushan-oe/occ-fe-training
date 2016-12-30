ad6e27d735af8f59741ec46ba22e179c42b583c6
callegas <callegas.f@gmail.com>
2016-12-30 13:28:43 -0200

    Apply recommended modifications in OE Hero Banner Callegas

--- storefront/widgets/objectedge/oeHeroBannerCallegas/js/widgetGeneric.js
+++ storefront/widgets/objectedge/oeHeroBannerCallegas/js/widgetGeneric.js
@@ -23,93 +23,91 @@ define(
         widgetModel.loopCarousel = ko.observable(widgetModel.boolLoopCarousel());
         widgetModel.autoPlayCarousel = ko.observable(widgetModel.boolAutoplayCarousel());
 
-        widgetModel.autoPlay = function() {
-        if (widgetModel.autoPlayCarousel()) {
-          $('.carousel').carousel({
-           pause:false
-         });
+        widgetModel.autoPlay = function(element) {
+          if (widgetModel.autoPlayCarousel()) {
+            $(element).carousel({
+              pause:false
+            });
+          }
         }
-      }
       }
 
       function itemCarousel(src, msrc, text, link, position) {
         this.src = '/file/general/' + src;
-        this.msrc = '/file/general/' + msrc;
-        this.text = text;
-        this.link = link;
-        this.position = position;
-        this.classActive = (position === 0) ? "active" : "";
-      }
+        if (msrc !== ""){ 
+         this.msrc = '/file/general/' + msrc; 
+       }
+       this.msrc = msrc ? '/file/general/' + msrc : null;
+       this.text = text;
+       this.link = link;
+       this.position = position;
+       this.classActive = (position === 0) ? "active" : "";
+     }
 
-      function VerifyFileExist(url, i) {
-        var http = $.ajax({
-          type: "HEAD",
-          url: "/file/general/" + url,
-          async: true,
-          success: function(response) {
-            addItem(i);
+     function verifyFileExist(url, i) {
+      var http = $.ajax({
+        type: "HEAD",
+        url: "/file/general/" + url,
+        async: true,
+        success: function(response) {
+          addItem(i);
+        }
+      });
+    }
+
+    function addItem(i) {
+      widgetModel.images.push(new itemCarousel(
+        widgetModel[imageUrl + i](), 
+        widgetModel[mobileImageUrl + i](),
+        widgetModel[bannerText + i](),
+        widgetModel[bannerLink + i](),
+        i - 1
+        ));
+    }
+
+    function statusDate(startDate, endDate){
+      startDate     = moment(startDate, "YYYY-MM-DD");
+      endDate       = moment(endDate, "YYYY-MM-DD");
+      var nowDate   = moment();
+
+      if(startDate === ""){
+        startDate = moment();
+      }
+      if(endDate === ""){
+        endDate = moment();
+      }
+      if((nowDate.isAfter(startDate,'day') || nowDate.isSame(startDate, 'day')) && (nowDate.isBefore(endDate, 'day') || nowDate.isSame(endDate, 'day'))){
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    return {
+      constructor : Generic,
+      config : ko.observable(),
+
+      onLoad : function(widget){
+        init(widget);
+        widgetModel.images = ko.observableArray();
+
+        for(var i=1; i<=6; i++){
+          if(widgetModel[imageUrl + i]() && statusDate(widget[startDate + i](), widget[endDate + i]())){
+            verifyFileExist(widgetModel[imageUrl + i](), i);
           }
-        });
-      }
-
-      function addItem(i) {
-        widgetModel.images.push(new itemCarousel(
-          widgetModel[imageUrl + i](), 
-          widgetModel[mobileImageUrl + i](),
-          widgetModel[bannerText + i](),
-          widgetModel[bannerLink + i](),
-          i - 1
-          ));
-      }
-
-      function statusDate(startDate, endDate){
-        startDate     = moment(startDate, "YYYY-MM-DD");
-        endDate       = moment(endDate, "YYYY-MM-DD");
-        var nowDate   = moment();
-
-        if(startDate == ""){
-          startDate = moment();
         }
-        if(endDate == ""){
-          endDate = moment();
-        }
-        if((nowDate.isAfter(startDate,'day') || nowDate.isSame(startDate, 'day')) && (nowDate.isBefore(endDate, 'day') || nowDate.isSame(endDate, 'day'))){
-          return true;
-        }
-        else {
-          return false;
-        }
-      }
 
-      return {
-        constructor : Generic,
-        config : ko.observable(),
+        widgetModel.__run('onLoad', widget);
+      },
 
-        onLoad : function(widget){
-          init(widget);
-          widgetModel.images = ko.observableArray();
+      beforeAppear : function(page){
+        widgetModel.__run('beforeAppear', page);
+      },
 
-          for(var i=1; i<=6; i++){
-            if(widgetModel[imageUrl + i]() && statusDate(widget[startDate + i](), widget[endDate + i]())){
-              VerifyFileExist(widgetModel[imageUrl + i](), i);
-            }
-          }
+    };
+  })();
 
-          //Provisional
-          // setTimeout(function() {
-          //   autoPlay();
-          // }, 2800);
-          
-          widgetModel.__run('onLoad', widget);
-        },
-
-        beforeAppear : function(page){
-          widgetModel.__run('beforeAppear', page);
-        },
-
-      };
-    })();
-
-    return new Generic();
-  }
-  );
+  return new Generic();
+}
+);

8720b0c49e8e10dc5569134470438fa42acc7d4f
sagar.shinkar <sagar.shinkar@objectedge.com>
2017-01-24 16:46:04 +0530

    TestWidget added.

---
+++ storefront/widgets_git/objectedge/TestWidget/js/TestWidget.js
@@ -0,0 +1,32 @@
+define(
+
+  //-------------------------------------------------------------------
+  // DEPENDENCIES
+  //-------------------------------------------------------------------
+  ['knockout', 'pubsub', 'ccConstants', 'koValidate', 'ccKoValidateRules', 'CCi18n'], 
+
+  //-------------------------------------------------------------------
+  // MODULE DEFINITION HERE
+  //-------------------------------------------------------------------
+  function(ko, pubsub, CCConstants, koValidate, rules, CCi18n) {
+
+    "use strict";
+
+    return {
+
+      /**
+       * beforeAppear function
+       */
+      beforeAppear: function (page) {
+      },
+
+      /**
+       * onLoad function
+       */
+      onLoad: function(widget) {
+
+      },
+
+    };
+  }
+);

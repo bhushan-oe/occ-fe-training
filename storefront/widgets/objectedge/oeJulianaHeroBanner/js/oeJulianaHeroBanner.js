69d8ac4f044a214b14711ccd23620565f8d0224a
Juliana Moreira <juliana.moreira@objectedge.com>
2017-03-02 19:18:37 -0300

    OET-16 : Finishing hero banner

--- storefront/widgets/objectedge/oeJulianaHeroBanner/js/oeJulianaHeroBanner.js
+++ storefront/widgets/objectedge/oeJulianaHeroBanner/js/oeJulianaHeroBanner.js
@@ -7,12 +7,12 @@ define(
   //-------------------------------------------------------------------
   // DEPENDENCIES
   //-------------------------------------------------------------------
-  ['knockout', 'jquery'],
+  ['knockout', 'jquery', 'moment'],
 
   //-------------------------------------------------------------------
   // MODULE DEFINITION
   //-------------------------------------------------------------------
-  function (ko, $) {
+  function (ko, $, moment) {
 
     "use strict";
 
@@ -21,103 +21,87 @@ define(
       /**
        * This function will run just on time when the widget was loaded the first time
        */
-      onLoad: function(widget) {
+      onLoad: function(widget) { 
         console.log(widget);
-        console.log("AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE2");
-        widget.firstName = "Bert";
-        // console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",widget.sampleText());
+        widget.currentDate = moment().format('YYYYMMDD');
 
-        /* ----------------- Temporary code ----------------- */
-          widget.imageUrl1       = "http://www.creativefan.com/files/2010/02/29475-fullsize-500x300.jpg";
-          widget.mobileImageUrl1 = "";
-          widget.bannerLink1     = "http://google.com";
-          widget.startDate1      = "";
-          widget.endDate1        = "";
-          widget.bannerText1     = "";
-
-          widget.imageUrl2       = "http://www.creativefan.com/files/2010/04/01939_norwegiancoast_800x480-500x300.jpg";
-          widget.mobileImageUrl2 = "";
-          widget.bannerLink2     = "http://deviantart.com";
-          widget.startDate2      = "";
-          widget.endDate2        = "";
-          widget.bannerText2     = "Hello";
-
-          widget.imageUrl3       = "";
-          widget.mobileImageUrl3 = "";
-          widget.bannerLink3     = "";
-          widget.startDate3      = "";
-          widget.endDate3        = "";
-          widget.bannerText3     = "";
-
-          widget.imageUrl4       = "";
-          widget.mobileImageUrl4 = "";
-          widget.bannerLink4     = "";
-          widget.startDate4      = "";
-          widget.endDate4        = "";
-          widget.bannerText4     = "";
-
-          widget.imageUrl5       = "";
-          widget.mobileImageUrl5 = "";
-          widget.bannerLink5     = "";
-          widget.startDate5      = "";
-          widget.endDate5        = "";
-          widget.bannerText5     = "";
-
-          widget.imageUrl6       = "";
-          widget.mobileImageUrl6 = "";
-          widget.bannerLink6     = "";
-          widget.startDate6      = "";
-          widget.endDate6        = "";
-          widget.bannerText6     = "";
-        /* ----------------- End Temporary code ----------------- */
-
-        console.log(">>>>>>>>>>>>>>>>>>>>>>>> ", widget.imageUrl1, " - ", widget["imageUrl"+"1"]);
-
-        widget.bannerItems = ko.observableArray([
+        //Populate the array with custom data from config.json
+        widget.bannerData = ko.observableArray([
           { 
-            imageUrl:       widget.imageUrl1,
-            mobileImageUrl: widget.mobileImageUrl1,
-            bannerLink:     widget.bannerLink1,
-            startDate:      widget.startDate1,
-            endDate:        widget.endDate1,
-            bannerText:     widget.bannerText1
+            imageUrl:       widget.imageUrl1.src(),
+            mobileImageUrl: widget.mobileImageUrl1.src(),
+            bannerLink:     widget.bannerLink1(),
+            startDate:      widget.startDate1(),
+            endDate:        widget.endDate1(),
+            bannerText:     widget.bannerText1()
+          }, {
+            imageUrl:       widget.imageUrl2.src(),
+            mobileImageUrl: widget.mobileImageUrl2.src(),
+            bannerLink:     widget.bannerLink2(),
+            startDate:      widget.startDate2(),
+            endDate:        widget.endDate2(),
+            bannerText:     widget.bannerText2()
           }, { 
-            imageUrl:       widget.imageUrl2,
-            mobileImageUrl: widget.mobileImageUrl2,
-            bannerLink:     widget.bannerLink2,
-            startDate:      widget.startDate2,
-            endDate:        widget.endDate2,
-            bannerText:     widget.bannerText2
+            imageUrl:       widget.imageUrl3.src(),
+            mobileImageUrl: widget.mobileImageUrl3.src(),
+            bannerLink:     widget.bannerLink3(),
+            startDate:      widget.startDate3(),
+            endDate:        widget.endDate3(),
+            bannerText:     widget.bannerText3()
           }, { 
-            imageUrl:       widget.imageUrl3,
-            mobileImageUrl: widget.mobileImageUrl3,
-            bannerLink:     widget.bannerLink3,
-            startDate:      widget.startDate3,
-            endDate:        widget.endDate3,
-            bannerText:     widget.bannerText3
+            imageUrl:       widget.imageUrl4.src(),
+            mobileImageUrl: widget.mobileImageUrl4.src(),
+            bannerLink:     widget.bannerLink4(),
+            startDate:      widget.startDate4(),
+            endDate:        widget.endDate4(),
+            bannerText:     widget.bannerText4()
           }, { 
-            imageUrl:       widget.imageUrl4,
-            mobileImageUrl: widget.mobileImageUrl4,
-            bannerLink:     widget.bannerLink4,
-            startDate:      widget.startDate4,
-            endDate:        widget.endDate4,
-            bannerText:     widget.bannerText4
+            imageUrl:       widget.imageUrl5.src(),
+            mobileImageUrl: widget.mobileImageUrl5.src(),
+            bannerLink:     widget.bannerLink5(),
+            startDate:      widget.startDate5(),
+            endDate:        widget.endDate5(),
+            bannerText:     widget.bannerText5()
           }, { 
-            imageUrl:       widget.imageUrl5,
-            mobileImageUrl: widget.mobileImageUrl5,
-            bannerLink:     widget.bannerLink5,
-            startDate:      widget.startDate5,
-            endDate:        widget.endDate5,
-            bannerText:     widget.bannerText5
-          }, { 
-            imageUrl:       widget.imageUrl6,
-            mobileImageUrl: widget.mobileImageUrl6,
-            bannerLink:     widget.bannerLink6,
-            startDate:      widget.startDate6,
-            endDate:        widget.endDate6,
-            bannerText:     widget.bannerText6
+            imageUrl:       widget.imageUrl6.src(),
+            mobileImageUrl: widget.mobileImageUrl6.src(),
+            bannerLink:     widget.bannerLink6(),
+            startDate:      widget.startDate6(),
+            endDate:        widget.endDate6(),
+            bannerText:     widget.bannerText6()
           }
         ]);
+
+        //Filter the array to show only valid items
+        widget.bannerItems = ko.computed(function() {
+          var filtered = ko.utils.arrayFilter(widget.bannerData(), function (item) {
+            //If the imageUrl exist
+            if(item.imageUrl) {
+              //If startDate and endDate were provided
+              if(item.startDate && item.endDate) {
+                //If currentDate exist between startDate and endDate
+                if(widget.currentDate >= item.startDate && widget.currentDate <= item.endDate) {
+                  return item;
+                }
+              //If exist only startDate and currentDate is equal or after startDate
+              } else if(item.startDate) {
+                if(widget.currentDate >= item.startDate) {
+                  return item;
+                }
+              //If exist only endDate and currentDate is equal or before endDate
+              } else if(item.endDate) {
+                if(widget.currentDate <= item.endDate) {
+                  return item;
+                }
+              //If startDate and endDate don't exist
+              } else {
+                return item;
+              }
+            }
+          });
+           
+          return filtered;
+        });
       },
 
       /**
@@ -125,24 +109,7 @@ define(
        */
       beforeAppear: function(page) {
 
-        // var selfWidget = this;
-
-        
-
-        /*
-          Deve ter as seguintes configurações:
-          timeSwitchImage == interval
-          boolLoopCarousel == wrap
-          boolAutoplayCarousel == interval with false
-
-          imageUrl
-          mobileImageUrl
-          bannerLink
-          startDate
-          endDate
-          bannerText
-        */
-
+        var selfWidget = this;
         /*
           ------------- Instructions to configure the carousel -------------
 
@@ -163,8 +130,8 @@ define(
           $('.carousel').carousel({interval: timeSwitchImage, wrap: boolLoopCarousel});
         }
 
-        generateCarousel();
-
+        generateCarousel(selfWidget.timeSwitchImage(), selfWidget.boolLoopCarousel(), selfWidget.boolAutoplayCarousel());
+        
       }
     };
   }

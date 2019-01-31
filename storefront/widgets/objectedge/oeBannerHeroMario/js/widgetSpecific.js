d08ae6f7aa0d8d043d87d14fb9b0056a78e4b29f
paribus <oiram.araujo@gmail.com>
2019-01-31 10:44:40 -0200

    OET-73: Widget Hero Banner developed based on the Helton widget.

---
+++ storefront/widgets/objectedge/oeBannerHeroMario/js/widgetSpecific.js
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview oeWidget
+ *
+ * @author @dev
+ */
+define(
+  //-------------------------------------------------------------------
+  // DEPENDENCIES
+  //-------------------------------------------------------------------
+  ['knockout', 'jquery'],
+  //-------------------------------------------------------------------
+  // MODULE DEFINITION
+  //-------------------------------------------------------------------
+  function(ko, $){
+    function Specific(){}
+
+    Specific.prototype = (function(){
+      var widgetModel;
+
+      function init(widget){
+        widgetModel = widget;
+      }
+
+      return {
+        // Obligatory
+        constructor : Specific,
+
+        // Variables
+
+        // Specific version
+        __OE__onLoad : function(widget){
+          init(widget);
+        },
+        __OE__beforeAppear : function(page){
+          
+        }
+      };
+    })();
+
+    return new Specific();
+  }
+);
